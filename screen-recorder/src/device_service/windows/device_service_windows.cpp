09343b06acec9b9676660dcf01fd2021387918fd
Benedetto <s247082@studenti.polito.it>
2022-06-09 15:48:47 +0200

    Initial commit

---
+++ screen-recorder/src/device_service/windows/device_service_windows.cpp
@@ -0,0 +1,12 @@
+#include "../../../include/device_service.h"
+#include <vector>
+
+std::vector<InputDeviceVideo> DeviceService::get_input_video_devices() {
+    std::vector<InputDeviceVideo> devices;
+    return devices;
+}
+
+std::vector<InputDeviceAudio> DeviceService::get_input_audio_devices() {
+    std::vector<InputDeviceAudio> devices;
+    return devices;
+}
